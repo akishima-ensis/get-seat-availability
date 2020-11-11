d564c355f1a2f9d935f33aa8eaab45336585288e
0x0u <m0zurillex@gmail.com>
2020-11-11 11:27:58 +0900

    :sparkles: firestoreに保存するデータを辞書からリストに変更

--- main.py
+++ main.py
@@ -8,69 +8,75 @@ from datetime import datetime, timedelta, timezone
 
 target_url = 'https://webreserv.library.akishima.tokyo.jp/webReserv/AreaInfo/Login'
 
-seat_data = {
-    # - seat_id : 部屋のid
-    #   - name: 部屋の名前
-    #   - status_code: 部屋の状態（0: 空席有, 1: 満席, 2: 開館前, 3: 休館日 4: データ取得の失敗）
-    #   - seats_num: 空席数
-    #   - web_seats_num: web予約可能座席数
-    #   - total_seats_num: 総座席数
-    #   - update: サイト内更新時間
+seats = [
+    # - id : 部屋のid
+    # - name: 部屋の名前
+    # - status_code: 部屋の状態（0: 空席有, 1: 満席,  2: 閉館, 3: 開館前, 4: 休館日 5: データ取得の失敗）
+    # - seats_num: 空席数
+    # - web_seats_num: web予約可能座席数
+    # - total_seats_num: 総座席数
+    # - update: サイト内更新時間
 
-    '1': {
+    {
+        'id': 0,
         'name': '学習席（有線LAN有）',
-        'status_code': 4,
+        'status_code': 5,
         'seats_num': 0,
         'web_seats_num': 0,
         'total_seats_num': 0,
         'update': '0000/00/00 00:00'
     },
 
-    '2': {
+    {
+        'id': 1,
         'name': '学習席',
-        'status_code': 4,
+        'status_code': 5,
         'seats_num': 0,
         'web_seats_num': 0,
         'total_seats_num': 0,
         'update': '0000/00/00 00:00'
     },
 
-    '3': {
+    {
+        'id': 2,
         'name': '研究個室',
-        'status_code': 4,
+        'status_code': 5,
         'seats_num': 0,
         'web_seats_num': 0,
         'total_seats_num': 0,
         'update': '0000/00/00 00:00'
     },
 
-    '4': {
+    {
+        'id': 3,
         'name': 'インターネット・DB席',
-        'status_code': 4,
+        'status_code': 5,
         'seats_num': 0,
         'web_seats_num': 0,
         'total_seats_num': 0,
         'update': '0000/00/00 00:00'
     },
 
-    '5': {
+    {
+        'id': 4,
         'name': 'グループ学習室',
-        'status_code': 4,
+        'status_code': 5,
         'seats_num': 0,
         'web_seats_num': 0,
         'total_seats_num': 0,
         'update': '0000/00/00 00:00'
     },
 
-    '6': {
+    {
+        'id': 5,
         'name': 'ティーンズ学習室',
-        'status_code': 4,
+        'status_code': 5,
         'seats_num': 0,
         'web_seats_num': 0,
         'total_seats_num': 0,
         'update': '0000/00/00 00:00'
-    },
-}
+    }
+]
 
 
 def get_seat_data():
@@ -93,31 +99,36 @@ def get_seat_data():
     update_strptime = datetime.strptime(''.join(update_str_re), '%Y%m%d%H%M')
     update = update_strptime.strftime('%Y/%m/%d %H:%M')
 
-    for seat_id in seat_data:
-        seats_data = [i.text for i in data[int(seat_id)].find_all('div')]
+    for seat_dict in seats:
+        seats_data = [i.text for i in data[int(seat_dict['id']) + 1].find_all('div')]
+
+        # 各部屋のdictionaryを取得
+        seat = seats[seat_dict['id']]
 
         # 座席ステータス
         if seats_data[0] == '満\u3000席':
-            seat_data[seat_id]['status_code'] = 1
+            seat['status_code'] = 1
+        elif seats_data[0] == '閉\u3000館':
+            seat['status_code'] = 2
         elif seats_data[0] == '開館前':
-            seat_data[seat_id]['status_code'] = 2
+            seat['status_code'] = 3
         elif seats_data[0] == '休館日':
-            seat_data[seat_id]['status_code'] = 3
+            seat['status_code'] = 4
         else:
-            seat_data[seat_id]['status_code'] = 0
+            seat['status_code'] = 0
 
             # 空席数
-            seat_data[seat_id]['seats_num'] = int(seats_data[0])
+            seat['seats_num'] = int(seats_data[0])
 
         # web空き情報
         if seats_data[1] != '':
-            seat_data[seat_id]['web_seats_num'] = int(seats_data[1])
+            seat['web_seats_num'] = int(seats_data[1])
 
         # 座席総数
-        seat_data[seat_id]['total_seats_num'] = int(seats_data[2])
+        seat['total_seats_num'] = int(seats_data[2])
 
         # サイト内更新時間
-        seat_data[seat_id]['update'] = update
+        seat['update'] = update
 
 
 def save_seat_data_to_firestore(Request):
@@ -140,9 +151,9 @@ def save_seat_data_to_firestore(Request):
     doc_ref = db.collection('seat').document(date)
     doc = doc_ref.get()
     if doc.exists:
-        db.collection('seat').document(date).update({time: seat_data})
+        db.collection('seat').document(date).update({time: seats})
     else:
-        db.collection('seat').document(date).set({time: seat_data})
+        db.collection('seat').document(date).set({time: seats})
 
     return 'ok'
 
